49ae435ab1084616d4efdb6cc230ea3146bec745
XZ-311\Efun <kenjian@efun.com>
2018-03-15 16:23:15 +0800

    使用Spring AOP的@Around环绕方式实现

--- src/main/java/com/simulation/my/Application.java
+++ src/main/java/com/simulation/my/Application.java
@@ -3,6 +3,7 @@ package com.simulation.my;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * 启动类
@@ -11,11 +12,12 @@ import org.springframework.context.annotation.ComponentScan;
  * @date 2018/11/17
  */
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.simulation.my"})
+@ComponentScan(basePackages = { "com.simulation.my" })
+@EnableAspectJAutoProxy
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
