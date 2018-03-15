aff12e1b7a4b53b1854b42c9fa5b0a4f015d4ebb
jiandeheng <623225956@qq.com>
2018-03-15 21:43:57 +0800

    使用spring-aop实现了请求日志的输出

--- src/main/java/com/simulation/my/aspect/RequestLogAspect.java
+++ src/main/java/com/simulation/my/aspect/RequestLogAspect.java
@@ -1,10 +1,8 @@
 package com.simulation.my.aspect;
 
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -16,53 +14,26 @@ import java.util.Enumeration;
 @Component
 public class RequestLogAspect {
 
-    private Long beforeTime;
-
-    private Long afterTime;
-
     @Pointcut("execution(public * com.simulation.my.controller.*.*(..))")
     public void requestLog() {
 
     }
 
-    @Before("requestLog()")
-    public void doBefore(JoinPoint joinPoint) {
-        // 输出参数
-        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = attributes.getRequest();
+    @Around(value = "requestLog()")
+    public Object Around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         Enumeration<String> requestNames = request.getParameterNames();
-        System.out.println("before:");
         System.out.println("请求参数如下：(key:value)");
         while (requestNames.hasMoreElements()) {
             String key = requestNames.nextElement();
             String value = request.getParameter(key);
             System.out.println(key + " : " + value);
         }
-        // 计时开始
-        beforeTime = System.currentTimeMillis();
+        Long beginTime = System.currentTimeMillis();
+        Object result = proceedingJoinPoint.proceed();
+        Long endTime = System.currentTimeMillis();
+        System.out.println("耗时：" + (endTime - beginTime) + "ms");
+        return result;
     }
 
-    @After("requestLog()")
-    public void doAfter(JoinPoint joinPoint) {
-        // 计时结束
-        afterTime = System.currentTimeMillis();
-        System.out.println("after:");
-        System.out.println("耗时: " + (afterTime-beforeTime) + "ms");
-    }
-
-    public Long getBeforeTime() {
-        return beforeTime;
-    }
-
-    public void setBeforeTime(Long beforeTime) {
-        this.beforeTime = beforeTime;
-    }
-
-    public Long getAfterTime() {
-        return afterTime;
-    }
-
-    public void setAfterTime(Long afterTime) {
-        this.afterTime = afterTime;
-    }
 }
